1b990e58c396f4c0633d12f430a4b8c82b193a20
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-08 10:30:10 +0200

    Added the calculator php class

--- POO/class.php
+++ POO/class.php
@@ -1,14 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Classe Film</title>
-</head>
-<body>
-    <?php 
-        echo "hej";
-    ?>
-</body>
-</html>
+<?php 
+class Film {
+    public $id;
+    public $nom;
+    public $realisateur;
+    public $annee;
+
+    function constructor($id, $nom, $realisateur, $annee) {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->realisateur = $realisateur;
+        $this->annee = $annee;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function getId() {
+        return $this->id;
+    }
+
+    function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    function getNom() {
+        return $this->nom;
+    }
+
+    function setRealisateur($realisateur) {
+        $this->realisateur = $realisateur;
+    }
+
+    function getRealisateur() {
+        return $this->realisateur;
+    }
+
+    function setAnnee($annee) {
+        $this->annee = $annee;
+    }
+
+    function getAnne() {
+        return $this->annee;
+    }
+}
+
+$film = new Film();
+$film->setId(1);
+$film->setNom('Pursue of Happiness');
+$film->setRealisateur('Will Smith');
+$film->setAnnee(2000);
+
+echo "The film " . $film->getNom() . " created by: " . $film->getRealisateur() . " year " . $film->getAnne();
