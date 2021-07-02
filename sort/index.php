ed95a41456dda99da0d3d5f02d854b4122c2fa9b
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-07-02 10:58:26 +0200

    filtering galerry js

---
+++ sort/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>filterable gallery</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+
+	<section class="gallery">
+		<div class="container">
+			<div class="row">
+				<div class="gallery-filter">
+					<span class="filter-item active" data-filter="all">all</span>
+					<span class="filter-item" data-filter="summer">summer</span>
+					<span class="filter-item" data-filter="autumn">autumn</span>
+					<span class="filter-item" data-filter="winter">winter</span>
+					<span class="filter-item" data-filter="spring">spring</span>
+				</div>
+			</div>
+			<div class="row">
+				<!-- gallery item start -->
+				<div class="gallery-item summer">
+					<div class="gallery-item-inner">
+						<img src="images/beach-1236581_640.jpg" alt="summer">
+					</div>
+				</div>
+				<!-- gallery item end -->
+				<!-- gallery item start -->
+				<div class="gallery-item summer">
+					<div class="gallery-item-inner">
+						<img src="images/beach-3369140_640.jpg" alt="summer">
+					</div>
+				</div>
+				<!-- gallery item end -->
+				<!-- gallery item start -->
+				<div class="gallery-item autumn">
+					<div class="gallery-item-inner">
+						<img src="images/benches-560435_640.jpg" alt="autumn">
+					</div>
+				</div>
+				<!-- gallery item end -->
+				<!-- gallery item start -->
+				<div class="gallery-item spring">
+					<div class="gallery-item-inner">
+						<img src="images/country-road-407207_640.jpg" alt="spring">
+					</div>
+				</div>
+				<!-- gallery item end -->
+				<!-- gallery item start -->
+				<div class="gallery-item summer">
+					<div class="gallery-item-inner">
+						<img src="images/indiana-dunes-state-park-1848559_640.jpg" alt="summer">
+					</div>
+				</div>
+				<!-- gallery item end -->
+				<!-- gallery item start -->
+				<div class="gallery-item winter">
+					<div class="gallery-item-inner">
+						<img src="images/lake-1581879_640.jpg" alt="winter">
+					</div>
+				</div>
+
+				<div class="gallery-item spring">
+					<div class="gallery-item-inner">
+						<img src="images/rain-2775032_640.jpg" alt="spring">
+					</div>
+				</div>
+
+				<div class="gallery-item autumn">
+					<div class="gallery-item-inner">
+						<img src="images/road-1072821_640.jpg" alt="autumn">
+					</div>
+				</div>
+
+				<div class="gallery-item autumn">
+					<div class="gallery-item-inner">
+						<img src="images/road-1072823_640.jpg" alt="autumn">
+					</div>
+				</div>
+
+				<div class="gallery-item winter">
+					<div class="gallery-item-inner">
+						<img src="images/snow-4668099_640.jpg" alt="winter">
+					</div>
+				</div>
+				<!-- gallery item end -->
+			</div>
+		</div>
+	</section>
+
+	<script src="main.js"></script>
+</body>
+
+</html>
